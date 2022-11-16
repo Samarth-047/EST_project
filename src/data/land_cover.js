4a2d176883e8603b401cfecccd4e9c427dc9adf3
khushidholiya <jay.b.ghevariya@gmail.com>
2022-11-17 00:20:58 +0530

    added land cover data for kachchh

--- src/data/land_cover.js
+++ src/data/land_cover.js
@@ -1,13 +1,17 @@
 import { Krishna_land_cover_headings, Krishna_land_cover_data } from "./mangroves/Krishna"
+import { Kachchh_land_cover_headings, Kachchh_land_cover_data } from "./mangroves/Kachchh"
 
 const land_cover = {
     headings: {
         'Krishna': Krishna_land_cover_headings,
+        'Kachchh': Kachchh_land_cover_headings,
     },
 
     data: {
         'Krishna': Krishna_land_cover_data,
+        'Kachchh': Kachchh_land_cover_data,
     }
+
 }
 
 export default land_cover;
